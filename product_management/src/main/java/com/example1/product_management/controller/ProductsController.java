d80bb0023afc170bc4e3a0d6496ed5027fe03abd
YersusZ <jhonsebastian.gil@utp.edu.co>
2026-06-07 18:10:53 -0500

    Borrado de import no utilizado

--- product_management/src/main/java/com/example1/product_management/controller/ProductsController.java
+++ product_management/src/main/java/com/example1/product_management/controller/ProductsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import com.example1.product_management.exceptions.NotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
